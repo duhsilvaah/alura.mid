e07575d23ad675e957a8ebeafaa23c2943022797
duhsilvaah <112639603+duhsilvaah@users.noreply.github.com>
2023-10-20 14:29:07 -0300

    ok

--- main.js
+++ main.js
@@ -40,6 +40,18 @@ function tocaSomPsh () {
 
 document.querySelector('.tecla_psh').onclick=tocaSomPsh;
 
+function tocaSomTic () {
+    document.querySelector('#som_tecla_tic').play();
+}
+
+document.querySelector('.tecla_tic').onclick=tocaSomTic;
+
+function tocaSomTom () {
+    document.querySelector('#som_tecla_tom').play();
+}
+
+document.querySelector('.tecla_tom').onclick=tocaSomTom;
+
 
 
 alert('Ola Duh!!! Seja Bem Vindo!!!');
